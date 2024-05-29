bc890c666b6260d7d2fde1d28ba9d4165f0e127b
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-05-29 22:19:32 +0530

    create head and tail node

---
+++ DoublyLinkedList.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+    struct node* prev;
+};
+
+struct node* head = NULL; //create head node
+struct node* tail = NULL; //create tail node
+
+int main(){
+
+    return 0;
+}
