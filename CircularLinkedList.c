3e05e0ac58ff384c4ff231612df37a798bec1403
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-02 22:01:29 +0530

    create node

--- CircularLinkedList.c
+++ CircularLinkedList.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-Struct node{
+struct node{
   int data;
   struct node* next;
 };
+
+struct node* head = NULL;
+
+struct node* createNewNode(int data)
+{
+    struct node* temp = (struct node*) malloc(sizeof(struct node));
+    if(temp == NULL){
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    temp->data = data;
+    temp->next = NULL;
+    return temp;   
+}
+
+int main(){
+  return 0;
+}
